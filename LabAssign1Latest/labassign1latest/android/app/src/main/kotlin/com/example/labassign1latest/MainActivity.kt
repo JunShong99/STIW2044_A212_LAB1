a7424dea60ec4fc678124b8f91c884fc7d55d530
JunShong99 <shongjunwei@gmail.com>
2022-04-25 19:22:01 +0800

    assignment1 269893

--- LabAssign1Latest/labassign1latest/android/app/src/main/kotlin/com/example/labassign1latest/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.labassign1latest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ LabAssignLatest1/labassignlatest1/android/app/src/main/kotlin/com/example/labassignlatest1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.labassignlatest1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
